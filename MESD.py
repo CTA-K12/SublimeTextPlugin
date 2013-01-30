5fdf96b263445e4afcc77b7bbcb4185a607c6b18
Oliver Forral <oforral@mesd.k12.or.us>
2013-01-30 13:39:30 -0800

    Added an example of a TextCommand

--- MESD.py
+++ MESD.py
@@ -4,3 +4,14 @@ class SaveandexitCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.window.run_command('save')
         self.window.run_command('close')
+
+class Rot13Command(sublime_plugin.TextCommand):
+    def run(self, view, args):
+        for region in view.sel():
+            if not region.empty():
+                # Get the selected text
+                s = view.substr(region)
+                # Transform it via rot13
+                s = s.encode('rot13')
+                # Replace the selection with transformed text
+                view.replace(region, s)

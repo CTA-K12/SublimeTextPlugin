ac462c806de3e2798680715dec47862c0ed85dd1
lighthart <dlighthart@mesd.k12.or.us>
2013-02-06 18:03:20 -0800

    Improved autoindenter for php

--- MESD.py
+++ MESD.py
@@ -1,5 +1,6 @@
 import string
 import sublime_plugin
+import re
 
 class SaveandcloseCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -8,8 +9,17 @@ class SaveandcloseCommand(sublime_plugin.WindowCommand):
 
 class ReindentallCommand(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.run_command('select_all')
+        #self.window.run_command('select_all')
+        self.window.run_command('oddspace')
         self.window.run_command('reindent')
+
+class FinishCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.run_command('select_all')
+        self.window.run_command('oddspace')
+        self.window.run_command('reindent')
+        self.window.run_command('save')
+        self.window.run_command('close')
 
 class Transformer(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -22,6 +32,34 @@ class Transformer(sublime_plugin.TextCommand):
 
             txt = f(view.substr(s))
             view.replace(edit, s, txt)
+
+class OddspaceCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        v = self.view
+        r = v.sel()
+        c = []
+        for s in r:
+            q=self.view.substr(s).split("\n")
+            for l in q:
+                k = (l.__len__() - l.lstrip().__len__()) % 4
+                if k:
+                    l=l[k:]
+                c.append(l)
+            v.replace(edit, s, "\n".join(c))
+
+#1
+ #1
+  #1
+   #1
+    #1
+     #1
+      #1
+       #1
+        #1
+         #1
+          #1
+
+
 
 def rot13(ch):
     o = ord(ch)

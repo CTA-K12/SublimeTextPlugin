c305b48733609a4523d16ca3ca627d9dcfd59b64
lighthart <dlighthart@mesd.k12.or.us>
2013-02-13 09:45:47 -0800

    Added color scheme selector

--- MESD.py
+++ MESD.py
@@ -1,6 +1,6 @@
 import string
-import sublime_plugin
-import re
+import sublime, sublime_plugin
+import re, os
 
 class SaveandcloseCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -9,7 +9,7 @@ class SaveandcloseCommand(sublime_plugin.WindowCommand):
 
 class ReindentallCommand(sublime_plugin.WindowCommand):
     def run(self):
-        #self.window.run_command('select_all')
+        self.window.run_command('select_all')
         self.window.run_command('oddspace')
         self.window.run_command('reindent')
 
@@ -71,3 +71,33 @@ def rot13(ch):
 
 class RotCommand(Transformer):
     transformer = lambda s: "".join([rot13(ch) for ch in s]),
+    
+class PrintThemeRule(sublime_plugin.TextCommand):
+
+  def load_theme(self):
+    theme = self.view.settings().get('color_scheme')
+    # strip the Packages/
+    theme = theme[9:]
+
+    path = os.path.join(sublime.packages_path(), theme)
+    with open(path, 'r') as f:
+      contents = f.read()
+
+    return contents
+
+  def parse_theme(self, theme):
+    return re.findall(r'<key>\s*scope\s*</key>\s*<string>([^<]+)</string>', theme)
+
+  def run(self, edit):
+    scope_name = self.view.scope_name(self.view.sel()[0].a)
+    selectors = self.parse_theme(self.load_theme())
+    best, bestScore = '', 0
+    for selector in selectors:
+      score = sublime.score_selector(scope_name, selector)
+      if score > bestScore:
+        bestScore = score
+        best = selector
+
+    str = "Selector: |%s| Scope: |%s| Score %d " % (best, scope_name, bestScore)
+    print str
+    sublime.status_message(str)
